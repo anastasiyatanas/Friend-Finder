139cb50aa48bb1951858b82332738e4e37004d37
BuildTools <unconfigured@null.spigotmc.org>
2019-01-25 17:29:44 -0700

    almost final

---
+++ app/data/friends.js
@@ -0,0 +1,124 @@
+var friends =[
+    {name: "Dwight",
+    photo: "images/dwight.jpg",
+    scores: [
+        "4",
+        "5",
+        "5",
+        "4",
+        "4",
+        "1",
+        "2",
+        "1",
+        "1",
+        "3"
+    ]
+    },
+    {name: "Creed",
+    photo: "images/creed.png",
+    scores: [
+        "1",
+        "3",
+        "1",
+        "4",
+        "4",
+        "1",
+        "3",
+        "2",
+        "4",
+        "4"
+    ]
+    },
+    {name: "Jim",
+    photo: "images/jim.jpg",
+    scores: [
+        "2",
+        "2",
+        "1",
+        "5",
+        "3",
+        "5",
+        "5",
+        "4",
+        "4",
+        "5"
+    ]
+    },
+    {name: "Micheal",
+    photo: "images/prison-mike.jpg",
+    scores: [
+        "5",
+        "5",
+        "1",
+        "5",
+        "1",
+        "2",
+        "4",
+        "5",
+        "5",
+        "5"
+    ]
+    },
+    {name: "Angela",
+    photo: "images/angela.jpg",
+    scores: [
+        "1",
+        "1",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+    ]
+    },
+    {name: "Meradeth",
+    photo: "images/meradith.jpg",
+    scores: [
+        "3",
+        "2",
+        "2",
+        "3",
+        "4",
+        "2",
+        "2",
+        "3",
+        "3",
+        "3"
+    ]
+    },
+    {name: "Erin",
+    photo: "images/erin.jpg",
+    scores: [
+        "5",
+        "5",
+        "2",
+        "5",
+        "1",
+        "4",
+        "3",
+        "5",
+        "5",
+        "5"
+    ]
+    },
+    {name: "Pam",
+    photo: "images/pam.jpg",
+    scores: [
+        "1",
+        "2",
+        "2",
+        "5",
+        "1",
+        "5",
+        "5",
+        "5",
+        "3",
+        "3"
+    ]
+    }
+];
+
+module.exports = friends;
